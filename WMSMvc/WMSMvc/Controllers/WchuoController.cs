123603f06c916302d235f76ecd67489a37b1d079
张春 <2934024192@qq.com>
2020-11-20 16:02:06 +0800

    我的

---
+++ WMSMvc/WMSMvc/Controllers/WchuoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMSMvc.Controllers
+{
+    public class WchuoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CeshiAdd()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Daily()
+        {
+            return View();
+        }
+    }
+}
