8026258cd38d31515cc291b9d8120bb36eb6ffcf
GuoKunnLong <2859705904@qq.com>
2020-11-26 15:34:45 +0800

    110

---
+++ WMSMvc/WMSMvc/Controllers/AllotController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMSMvc.Controllers
+{
+    public class AllotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AllotShow()
+        {
+            return View();
+        }
+        public IActionResult ItemdetailShow()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult AuditShow()
+        {
+            return View();
+        }
+        public IActionResult LookShow()
+        {
+            return View();
+        }
+        public IActionResult BHShow()
+        {
+            return View();
+        }
+        public IActionResult FaPeiShow()
+        {
+            return View();
+        }
+        public IActionResult DBRuKuShow()
+        {
+            return View();
+        }
+        public IActionResult NotarizeShow()//入库确定
+        {
+            return View();
+        }
+    }
+}
