52b7387e7c8a519acaea090e154ce5a430b6e878
ttf0512 <1799441665@qq.com>
2020-11-26 10:51:11 +0800

    over

---
+++ WMSMvc/WMSMvc/Controllers/PurchaseController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMSMvc.Controllers
+{
+    public class PurchaseController :Controller
+    {
+        public ActionResult Show()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Todo()
+        {
+            return View();
+        }
+        public ActionResult Ruku(string ids)
+        {
+            ViewBag.Is = ids;
+            return View();
+        }
+        public ActionResult Xiangqing(string ids)
+        {
+            ViewBag.Is = ids;
+            return View();
+        }
+    }
+}
--- WMSMvc/WMSMvc/Startup.cs
+++ WMSMvc/WMSMvc/Startup.cs
@@ -46,7 +46,7 @@ namespace WMSMvc
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Muban}/{action=Index}/{id?}");
             });
         }
     }
